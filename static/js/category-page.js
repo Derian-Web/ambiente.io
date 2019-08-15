697d80137fa000d936efe97d0f624bb868a3377c
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-08-15 13:55:11 -0500

    scroll personalizado

--- static/js/category-page.js
+++ static/js/category-page.js
@@ -1,69 +1,81 @@
 var sitios = [{
     nombre: "nombre1",
     desc: "descripcion1",
-    sitio: "https://envylabs.com/"
+    sitio: "https://envylabs.com/",
+    imagenRuta: "static/images/paginaEjemplo.png"
 },
 {
     nombre: "nombre2",
     desc: "descripcion2",
-    sitio: "http://builtwithmomentum.com/"
+    sitio: "http://builtwithmomentum.com/",
+    imagenRuta: "static/images/paginaEjemplo2.png"
 },
 {
     nombre: "nombre3",
     desc: "descripcion3",
-    sitio: "http://teixido.co/"
+    sitio: "http://teixido.co/",
+    imagenRuta: "static/images/paginaEjemplo3.png"
 },
 {
     nombre: "nombre4",
     desc: "descripcion4",
-    sitio: "https://createdm.com/"
+    sitio: "https://createdm.com/",
+    imagenRuta: "static/images/paginaEjemplo4.png"
 },
 {
     nombre: "nombre5",
     desc: "descripcion5",
-    sitio: "https://www.creuna.com/en/"
+    sitio: "https://www.creuna.com/en/",
+    imagenRuta: "static/images/paginaEjemplo2.png"
 },
 {
     nombre: "nombre6",
     desc: "descripcion6",
-    sitio: "https://www.plankdesign.com/en"
+    sitio: "https://www.plankdesign.com/en",
+    imagenRuta: "static/images/paginaEjemplo3.png"
 },
 {
     nombre: "nombre1",
     desc: "descripcion1",
-    sitio: "https://envylabs.com/"
+    sitio: "https://envylabs.com/",
+    imagenRuta: "static/images/paginaEjemplo4.png"
 },
 {
     nombre: "nombre2",
     desc: "descripcion2",
-    sitio: "http://builtwithmomentum.com/"
+    sitio: "http://builtwithmomentum.com/",
+    imagenRuta: "static/images/paginaEjemplo.png"
 },
 {
     nombre: "nombre3",
     desc: "descripcion3",
-    sitio: "http://teixido.co/"
+    sitio: "http://teixido.co/",
+    imagenRuta: "static/images/paginaEjemplo3.png"
 },
 {
     nombre: "nombre4",
     desc: "descripcion4",
-    sitio: "https://createdm.com/"
+    sitio: "https://createdm.com/",
+    imagenRuta: "static/images/paginaEjemplo4.png"
 },
 {
     nombre: "nombre5",
     desc: "descripcion5",
-    sitio: "https://www.creuna.com/en/"
+    sitio: "https://www.creuna.com/en/",
+    imagenRuta: "static/images/paginaEjemplo2.png"
 },
 {
     nombre: "nombre6",
     desc: "descripcion6",
-    sitio: "https://www.plankdesign.com/en"
+    sitio: "https://www.plankdesign.com/en",
+    imagenRuta: "static/images/paginaEjemplo.png"
 }];
 
 
 var contenido = "";
 sitios.forEach(element => {
-    contenido = document.getElementById('contenedorSitios').innerHTML += "<div class='col-md-4 pagina'><div><iframe class='embedded' frameborder='0' src='"+element.sitio+"' name='formularios'></iframe><div class='tarjeta'><h4>"+element.nombre+"</h4><p>"+element.desc+"</p><button type='button' onclick=window.open('"+element.sitio+"') class='btn btn-outline-dark'>Visit site</button></div></div></div>";
+    contenido = document.getElementById('contenedorSitios').innerHTML += "<div class='col-md-4 pagina'><div><div class='imagePage'><img src='"+ element.imagenRuta+"'></div><div class='tarjeta'><h4>"+element.nombre+"</h4><p>"+element.desc+"</p><button type='button' onclick=window.open('"+element.sitio+"') class='btn btn-outline-dark'>Visit site</button></div></div></div>";
 });
-console.log(sitios);
+
 
 
