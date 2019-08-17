a99383d1e8e5333ef3f30c5733f2a177ff45bed4
Ricardohp22 <45410177+Ricardohp22@users.noreply.github.com>
2019-08-16 21:09:11 -0500

    Category-page con datos traidos de API

--- static/js/category-page.js
+++ static/js/category-page.js
@@ -1,81 +1,42 @@
-var sitios = [{
-    nombre: "nombre1",
-    desc: "descripcion1",
-    sitio: "https://envylabs.com/",
-    imagenRuta: "static/images/paginaEjemplo.png"
-},
-{
-    nombre: "nombre2",
-    desc: "descripcion2",
-    sitio: "http://builtwithmomentum.com/",
-    imagenRuta: "static/images/paginaEjemplo2.png"
-},
-{
-    nombre: "nombre3",
-    desc: "descripcion3",
-    sitio: "http://teixido.co/",
-    imagenRuta: "static/images/paginaEjemplo3.png"
-},
-{
-    nombre: "nombre4",
-    desc: "descripcion4",
-    sitio: "https://createdm.com/",
-    imagenRuta: "static/images/paginaEjemplo4.png"
-},
-{
-    nombre: "nombre5",
-    desc: "descripcion5",
-    sitio: "https://www.creuna.com/en/",
-    imagenRuta: "static/images/paginaEjemplo2.png"
-},
-{
-    nombre: "nombre6",
-    desc: "descripcion6",
-    sitio: "https://www.plankdesign.com/en",
-    imagenRuta: "static/images/paginaEjemplo3.png"
-},
-{
-    nombre: "nombre1",
-    desc: "descripcion1",
-    sitio: "https://envylabs.com/",
-    imagenRuta: "static/images/paginaEjemplo4.png"
-},
-{
-    nombre: "nombre2",
-    desc: "descripcion2",
-    sitio: "http://builtwithmomentum.com/",
-    imagenRuta: "static/images/paginaEjemplo.png"
-},
-{
-    nombre: "nombre3",
-    desc: "descripcion3",
-    sitio: "http://teixido.co/",
-    imagenRuta: "static/images/paginaEjemplo3.png"
-},
-{
-    nombre: "nombre4",
-    desc: "descripcion4",
-    sitio: "https://createdm.com/",
-    imagenRuta: "static/images/paginaEjemplo4.png"
-},
-{
-    nombre: "nombre5",
-    desc: "descripcion5",
-    sitio: "https://www.creuna.com/en/",
-    imagenRuta: "static/images/paginaEjemplo2.png"
-},
-{
-    nombre: "nombre6",
-    desc: "descripcion6",
-    sitio: "https://www.plankdesign.com/en",
-    imagenRuta: "static/images/paginaEjemplo.png"
-}];
+var sitios = [];
+
+const urlApi = 'http://fundamentos.academlo.com/api/v1/directories/5c82982e-b63e-4280-8287-4eba0e99716a/categories';
+
+fetch(urlApi)
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(directories => {
+        console.log(directories);
+        directories.categories.forEach(elemento => {
+
+            var objeto = {
+                nombre: "nombre1",
+                desc: "descripcion1",
+                sitio: "https://envylabs.com/",
+                imagenRuta: "static/images/paginaEjemplo.png"
+            };
+            
+            objeto.nombre = elemento.name;
+            sitios.push(objeto);
+        });
+        //console.log(sitios);
+        sitios.forEach(element => {
+            document.getElementById('contenedorSitios').innerHTML += "<div class='col-md-4 pagina'><div><div class='imagePage'><img src='" + element.imagenRuta + "'></div><div class='tarjeta'><h4>" + element.nombre + "</h4><p>" + element.desc + "</p><button type='button' onclick=window.open('" + element.sitio + "') class='btn btn-outline-dark'>Visit site</button></div></div></div>";
+        });
+    })
+    .catch(error => {
+        alert("hubo un error");
+    })
 
 
-var contenido = "";
-sitios.forEach(element => {
-    contenido = document.getElementById('contenedorSitios').innerHTML += "<div class='col-md-4 pagina'><div><div class='imagePage'><img src='"+ element.imagenRuta+"'></div><div class='tarjeta'><h4>"+element.nombre+"</h4><p>"+element.desc+"</p><button type='button' onclick=window.open('"+element.sitio+"') class='btn btn-outline-dark'>Visit site</button></div></div></div>";
-});
+
+
+//var contenido = "";
+
+
+
 
 
 
